9f8a5088c8fae2b1dda487408483b9bef49fab5d
Adrian Molofsky <molofsky@stanford.edu>
2023-01-09 16:01:18 -0800

    Create prime.v

---
+++ prime.v
@@ -0,0 +1,12 @@
+module prime(in, isprime); 
+	input [3:0] in;
+	output 	isprime;
+	reg 		isprime;
+	
+	always @(in) begin
+		case(in)
+		1, 2 , 3, 5, 7, 11, 13: isprime = 1’b1;
+		default: isprime = 1’b0;
+		endcase
+	end
+endmodule
